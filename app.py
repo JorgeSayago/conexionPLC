8498746b234ad5d75e22cd1e6412e4aa521cda9d
jorge sayago <sayagojorge2003@gmail.com>
2025-07-28 10:31:44 -0500

    commitTabla
    creamos unatabla que reciba y muestre los datos del PLC

--- app.py
+++ app.py
@@ -1,2 +1,48 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
+import snap7
+from conecction.lectura import leer_datos
+
+# Mostrar logo arriba solo si existe
+try:
+    img = Image.open("./assets/logo.jpeg")
+    st.image(img, width=400)
+except FileNotFoundError:
+    pass
+
+st.set_page_config(page_title="Dashboard PLC")
+
+# Parametros de conexion
+IP = "192.168.1.10"
+RACK = 0
+SLOT = 1
+
+# CREAR CONEXION SOLO UNA VEZ
+@st.cache_resource
+def conectarPLC():
+    plc = snap7.client.Client()
+    plc.connect(IP, RACK, SLOT)
+    return plc 
+
+def main():
+    st.title("Recoleccion de datos PLC")
+
+    plc = conectarPLC()
+
+    if st.button("Leer datos del PLC"):
+        nuevo = leer_datos(plc)
+        if "datos" not in st.session_state:
+            st.session_state.datos = []
+
+            st.session_state.datos.append(nuevo)
+            st.success("Datos leidos")
+    
+    # mostrar en la tabla
+    if "datos" in st.session_state and st.session_state.datos:
+        df = pd.DataFrame(st.session_state.datos)
+        st.dataframe(df)
+
+
+if __name__ == "__main__":
+    main()
---
+++ conecction/lectura.py
@@ -0,0 +1,13 @@
+import snap7
+
+def leer_datos(plc, DB_NUMBER=2, START_ADRESS=0,SIZE=259):
+    db = plc.db_read(DB_NUMBER,START_ADRESS,SIZE)
+    product_name =db[2:256].decode('UTF-8').strip('\x00')
+    product_value = int.from_bytes(db[256:258], byteorder='big')
+    product_status = bool(db[258])
+
+    return{
+        "Producto": product_name,
+        "Valor": product_value,
+        "Estadp": product_status
+    }
